41699e54a2cbe29dacfa9421e2e6d1c7d135fa0c
Olivier Livet <oli.livet@gmail.com>
2019-10-29 12:13:43 +0100

    Update G-Config syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteUrl: 'https://netlify.olivierlivet.net',
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
+    siteUrl: 'https://netlify.olivierlivet.net',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
